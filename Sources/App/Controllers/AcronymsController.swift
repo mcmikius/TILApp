75002ef3c7f6ae8ed8fae093298f09610a81679e
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-29 15:05:04 +0200

    add handler for get post in controller

---
+++ Sources/App/Controllers/AcronymsController.swift
@@ -0,0 +1,26 @@
+//
+//  AcronymsController.swift
+//  App
+//
+//  Created by Mykhailo Bondarenko on 29.02.2020.
+//
+
+import Foundation
+import Vapor
+
+
+struct AcronymsController: RouteCollection {
+    func boot(router: Router) throws {
+        let acronymsRoute = router.grouped("api", "acronyms")
+        acronymsRoute.get(use: getAllHandler)
+        acronymsRoute.post(Acronym.self, use: createHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
+        return Acronym.query(on: req).all()
+    }
+    
+    func createHandler(_ req: Request, acronym: Acronym) throws -> Future<Acronym> {
+        return acronym.save(on: req)
+    }
+}

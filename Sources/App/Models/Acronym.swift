cc461fac333e0f67f422fef6866dd866c23ef25c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-29 14:53:43 +0200

    add model and extension Model

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,15 @@
+import FluentSQLite
+import Vapor
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+extension Acronym: SQLiteModel {}
+extension Acronym: Content {}

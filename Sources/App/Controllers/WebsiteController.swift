1e4a6b6d5dd3c7fe74592423196e8da0d0982526
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-02 11:55:13 +0200

    add categories and user pages and routers

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -13,6 +13,9 @@ struct WebsiteController: RouteCollection {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
         router.get("users", User.parameter, use: userHandler)
+        router.get("users", use: allUsersHandler)
+        router.get("categories", Category.parameter, use: categoryHandler)
+        router.get("categories", use: allCategoriesHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -25,7 +28,7 @@ struct WebsiteController: RouteCollection {
     func acronymHandler(_ req: Request) throws -> Future<View> {
         return try req.parameters.next(Acronym.self).flatMap(to: View.self, { acronym in
             return acronym.user.get(on: req).flatMap(to: View.self) { (user) in
-                let context = AcronymContext(title: acronym.long, acronym: acronym, user: user)
+                let context = try AcronymContext(title: acronym.long, acronym: acronym, user: user, categories: acronym.categories.query(on: req).all())
                 return try req.view().render("acronym", context)
             }
         })
@@ -36,6 +39,23 @@ struct WebsiteController: RouteCollection {
             let context = try UserContext(title: user.name, user: user, acronyms: user.acronyms.query(on: req).all())
             return try req.view().render("user", context)
         })
+    }
+    
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        let context = AllUsersContext(users: User.query(on: req).all())
+        return try req.view().render("allUsers", context)
+    }
+    
+    func categoryHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Category.self).flatMap(to: View.self, { category in
+            let context = try CategoryContext(title: category.name, category: category, acronyms: category.acronyms.query(on: req).all())
+            return try req.view().render("category", context)
+        })
+    }
+    
+    func allCategoriesHandler(_ req: Request) throws -> Future<View> {
+        let context = AllCategoriesContext(categories: Category.query(on: req).all())
+        return try req.view().render("allCategories", context)
     }
 }
 
@@ -48,10 +68,27 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+    let categories: Future<[Category]>
 }
 
 struct UserContext: Encodable {
     let title: String
     let user: User
     let acronyms: Future<[Acronym]>
+}
+
+struct AllUsersContext: Encodable {
+    let title: String = "All Users"
+    let users: Future<[User]>
+}
+
+struct CategoryContext: Encodable {
+    let title: String
+    let category: Category
+    let acronyms: Future<[Acronym]>
+}
+
+struct AllCategoriesContext: Encodable {
+    let title: String = "All Categories"
+    let categories: Future<[Category]>
 }

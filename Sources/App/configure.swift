d2fe12097a775b195b63d7985be684e73edc450f
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-02 11:06:32 +0200

    add bootstrap

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,31 +6,31 @@ import Leaf
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentMySQLProvider())
-//    try services.register(FluentSQLiteProvider())
+    //    try services.register(FluentSQLiteProvider())
     try services.register(LeafProvider())
-
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
+    
     // Configure a SQLite database
     let databaseConfig = MySQLDatabaseConfig(hostname: "localhost", username: "til", password: "password", database: "vapor")
     let database = MySQLDatabase(config: databaseConfig)
-//    let sqlite = try SQLiteDatabase(storage: .memory)
-
+    //    let sqlite = try SQLiteDatabase(storage: .memory)
+    
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: database, as: .mysql)
-//    databases.add(database: sqlite, as: .sqlite)
+    //    databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
-
+    
     // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Acronym.self, database: .mysql)
